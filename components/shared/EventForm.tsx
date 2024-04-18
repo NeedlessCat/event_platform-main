d55b46ece88b9587900b2f5982a2b9610595ae3e
NeedlessCat <kumar29.aayush@gmail.com>
2024-04-19 01:15:45 +0530

    h

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -49,6 +49,7 @@ const EventForm = ({ userId, type, event, eventId }: EventFormProps) => {
   })
  
   async function onSubmit(values: z.infer<typeof eventFormSchema>) {
+    // console.log(values);
     let uploadedImageUrl = values.imageUrl;
 
     if(files.length > 0) {
